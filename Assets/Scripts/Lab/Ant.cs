61648696173c17ecd2e890587f181ef6fad14852
KAKanna <mlg666pop@gmail.com>
2024-10-01 11:07:01 +0700

    ROCK

--- Assets/Scripts/Lab/Ant.cs
+++ Assets/Scripts/Lab/Ant.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movepoints;
+
+    public void Start()
+    {
+        Init(10);
+        Debug.Log($"Health Ant {Health}");
+
+        Behavior();
+    }
+    //moving not warp
+    public void FixedUpdate()
+    {
+        Behavior();
+    }
+    //Here is speed s=v*t
     public override void Behavior()
     {
-        Debug.Log("This is ant Behave");
+        //             point at now    speed          time(fixupdate)
+        //                  ^            ^                  ^
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (rb.position.x <= movepoints[0].position.x && velocity.x < 0)
+        {
+            Flip();
+        }
+        else if (rb.position.x >= movepoints[1].position.x && velocity.x > 0)
+        {
+            Flip();
+        }
+    }
+    public void Flip()
+    {
+        velocity *= -1;
+
+        Vector2 charScale = transform.localScale;
+        charScale.x *= -1;
+        transform.localScale = charScale;
     }
 }
--- Assets/Scripts/Lab/Crocodile.cs
+++ Assets/Scripts/Lab/Crocodile.cs
@@ -1,14 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Crocodile : Enemy
 {
-    private float attackRange;
-    private float player;
+    [SerializeField] private float attackRange;
+    [SerializeField] private Player player;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletWaitTime;
+    [SerializeField] private float bulletTimer;
 
+    public void Start()
+    {
+        Init(100);
+        Debug.Log($"Health Crocodile {Health}");
+
+        
+    }
+    public void Update()
+    {
+        bulletTimer -= Time.deltaTime;
+
+        Behavior();
+        
+        if(bulletTimer < 0 )
+        {
+            bulletTimer = bulletWaitTime;
+        }
+    }
     public override void Behavior()
     {
-        Debug.Log("This is Crocodile Behave");
+        Vector2 direction = player.transform.position - transform.position;
+        float distance =  direction.magnitude;
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
     }
 }
